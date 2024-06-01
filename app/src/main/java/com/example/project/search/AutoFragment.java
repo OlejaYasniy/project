4b8995a97c8841cc656da3f41e0e26b60a29871a
OlejaYasniy <oleg.grishaovich@mail.ru>
2024-06-02 00:17:56 +0300

    added search 2.0

---
+++ app/src/main/java/com/example/project/search/AutoFragment.java
@@ -0,0 +1,116 @@
+package com.example.project.search;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.project.MainActivity;
+import com.example.project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AutoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AutoFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private String autoBrand;
+
+    public AutoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AutoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AutoFragment newInstance(String param1, String param2) {
+        AutoFragment fragment = new AutoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+            autoBrand = getArguments().getString("auto_brand"); // добавлено
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_auto, container, false);
+        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+
+        int[] imageResources;
+        // Выбираем массив строк в зависимости от марки автомобиля
+        int stringArrayId = 0;
+        if (autoBrand.equalsIgnoreCase("BMW")) {
+            stringArrayId = R.array.Bmw;
+            actionBar.setTitle("BMW");
+            imageResources = new int[] {
+                    R.drawable.bmwm4,
+                    R.drawable.bmwx5,
+                    R.drawable.bmwm3s,
+            };;
+        } else if (autoBrand.equalsIgnoreCase("Audi")) {
+            stringArrayId = R.array.Audi;
+            actionBar.setTitle("Audi");
+            imageResources = new int[] {
+                    R.drawable.audirs6,
+                    R.drawable.audiq7,
+                    R.drawable.audia4,
+            };
+        } else if (autoBrand.equalsIgnoreCase("Mercedes-Benz")) {
+            stringArrayId = R.array.MercedesBenz;
+            actionBar.setTitle("Mercedes-Benz");
+            imageResources = new int[] {
+                    R.drawable.mbgt,
+                    R.drawable.mbgle,
+                    R.drawable.mbcclass,
+            };
+        } else {
+            Toast.makeText(getActivity(), "Invalid auto brand", Toast.LENGTH_SHORT).show();
+            return view;
+        }
+
+        String[] textArray = getResources().getStringArray(stringArrayId);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new SimpleAdapter(textArray, imageResources));
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/project/search/SimpleAdapter.java
@@ -0,0 +1,51 @@
+package com.example.project.search;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project.R;
+
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+    private final String[] textArray;
+    private final int[] imageResources;
+
+    public SimpleAdapter(String[] textArray, int[] imageResources) {
+        this.textArray = textArray;
+        this.imageResources = imageResources;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.auto_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(imageResources[position]);
+        holder.textView.setText(textArray[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return textArray.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_view);
+            textView = itemView.findViewById(R.id.text_view);
+        }
+    }
+}
